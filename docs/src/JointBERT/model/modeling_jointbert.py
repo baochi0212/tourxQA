18bbbc7058e05658bac34b4453e5f8d166d641ae
baochi0212 <chideptraika29@gmail.com>
2022-10-28 10:41:19 +0700

    JointBert

--- docs/src/JointBERT/model/modeling_jointbert.py
+++ docs/src/JointBERT/model/modeling_jointbert.py
@@ -1,11 +1,11 @@
 import torch
 import torch.nn as nn
-from transformers import BertPreTrainedModel, BertModel, BertConfig, AutoModel
+from transformers import BertPreTrainedModel, BertModel, BertConfig
 from torchcrf import CRF
 from .module import IntentClassifier, SlotClassifier
 
 
-class JointBERT(AutoModel):
+class JointBERT(BertPreTrainedModel):
     def __init__(self, config, args, intent_label_lst, slot_label_lst):
         super(JointBERT, self).__init__(config)
         self.args = args
