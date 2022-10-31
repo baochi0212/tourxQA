47de523dfbb079b0f39a8944c87532631e0ef903
baochi0212 <chideptraika29@gmail.com>
2022-10-31 23:18:54 +0700

    JointBert

--- docs/src/JointIDSF/run_jointIDSF_PhoBERTencoder.sh
+++ docs/src/JointIDSF/run_jointIDSF_PhoBERTencoder.sh
@@ -3,7 +3,7 @@
 #Train JointIDSF
 export lr=4e-5
 export c=0.5
-export s=100
+export s=1001
 echo "${lr}"
 export MODEL_DIR=JointIDSF_PhoBERTencoder
 export MODEL_DIR=$MODEL_DIR"/"$lr"/"$c"/"$s
@@ -24,7 +24,6 @@ python3 main.py --token_level word-level \
                   --gpu_id 0 \
                   --embedding_type soft \
                   --intent_loss_coef $c \
-        
                   --learning_rate $lr
 
 #   --tuning_metric mean_intent_slot \
