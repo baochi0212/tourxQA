a16459d4aaf7d845cc772fcc15450acf1563fce9
baochi0212 <chideptraika29@gmail.com>
2022-10-31 23:01:06 +0700

    JointBert

--- docs/src/JointIDSF/run_jointBERT-CRF_PhoBERTencoder.sh
+++ docs/src/JointIDSF/run_jointBERT-CRF_PhoBERTencoder.sh
@@ -5,21 +5,21 @@ echo "${lr}"
 export MODEL_DIR=JointBERT-CRF_PhoBERTencoder
 export MODEL_DIR=$MODEL_DIR"/"$lr"/"$c"/"$s
 echo "${MODEL_DIR}"
-python3 main.py --token_level word-level \
-                  --model_type phobert \
-                  --model_dir $MODEL_DIR \
-                  --data_dir PhoATIS \
-                  --seed $s \
-                  --do_train \
-                  --do_eval \
-                  --save_steps 140 \
-                  --logging_steps 140 \
-                  --num_train_epochs 2 \
-                #   --tuning_metric mean_intent_slot \
-                  --use_crf \
-                  --gpu_id 0 \
-                  --embedding_type soft \
-                  --intent_loss_coef $c \
-                  --learning_rate $lr
-                  --use_attention_mask
-# python3 main.py --token_level word-level --model_type phobert  --data_dir PhoATIS --seed $s --do_train --do_eval --save_steps 140 --logging_steps 140 --num_train_epochs 2 --use_crf --gpu_id 0 --embedding_type soft --intent_loss_coef $c --learning_rate $lr --use_attention_mask
+# python3 main.py --token_level word-level \
+#                   --model_type phobert \
+#                   --model_dir $MODEL_DIR \
+#                   --data_dir PhoATIS \
+#                   --seed $s \
+#                   --do_train \
+#                   --do_eval \
+#                   --save_steps 140 \
+#                   --logging_steps 140 \
+#                   --num_train_epochs 2 \
+#                 #   --tuning_metric mean_intent_slot \
+#                   --use_crf \
+#                   --gpu_id 0 \
+#                   --embedding_type soft \
+#                   --intent_loss_coef $c \
+#                   --learning_rate $lr
+#                   --use_attention_mask
+python3 main.py --token_level word-level --model_dir $MODEL_DIR --model_type phobert  --data_dir PhoATIS --seed $s --do_train --do_eval --save_steps 140 --logging_steps 140 --num_train_epochs 2 --use_crf --gpu_id 0 --embedding_type soft --intent_loss_coef $c --learning_rate $lr --use_attention_mask
