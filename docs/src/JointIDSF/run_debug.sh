bb02e175246c4c3d713231d82dae2b3d2bdc221c
baochi0212 <chideptraika29@gmail.com>
2022-11-01 09:49:22 +0700

    JointBert

--- docs/src/JointIDSF/run_debug.sh
+++ docs/src/JointIDSF/run_debug.sh
@@ -2,26 +2,13 @@
 # ./run_jointBERT-CRF_PhoBERTencoder.sh
 #Train JointIDSF
 export lr=5e-5
-export c=0.15
-export e=1
+export c=0.2
+export e=50
 export s=10001
 echo "${lr}"
 export MODEL_DIR=JointIDSF_PhoBERTencoder
 export MODEL_DIR=$MODEL_DIR"/"$lr"/"$c"/"$s
 echo "${MODEL_DIR}"
-python3 main.py --token_level word-level \
-                  --model_type phobert \
-                  --model_dir $MODEL_DIR \
-                  --data_dir PhoATIS \
-                  --seed $s \
-                  --do_train \
-                  --do_eval \
-                  --save_steps 140 \
-                  --logging_steps 140 \
-                  --num_train_epochs $e\
-                  --tuning_metric loss \
-                  --use_crf \
-
 python3 main.py --token_level word-level \
                   --model_type phobert \
                   --model_dir $MODEL_DIR \
@@ -39,3 +26,4 @@ python3 main.py --token_level word-level \
                   --embedding_type soft \
                   --intent_loss_coef $c \
                   --learning_rate $lr
+                  --use_crf \
