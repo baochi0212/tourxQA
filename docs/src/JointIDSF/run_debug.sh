b43dde6c93a8133f2cc0208c07fea6e9e59af63e
baochi0212 <chideptraika29@gmail.com>
2022-11-01 08:48:40 +0700

    JointBert

--- docs/src/JointIDSF/run_debug.sh
+++ docs/src/JointIDSF/run_debug.sh
@@ -17,7 +17,26 @@ python3 main.py --token_level word-level \
                   --do_eval \
                   --save_steps 140 \
                   --logging_steps 140 \
-                  --num_train_epochs 50 \
+                  --num_train_epochs 3 \
+                  --tuning_metric mean_intent_slot \
+
+                  --attention_embedding_size 200 \
+                  --use_crf \
+                  --gpu_id 0 \
+                  --embedding_type soft \
+                  --intent_loss_coef $c \
+                  --learning_rate $lr
+
+python3 main.py --token_level word-level \
+                  --model_type phobert \
+                  --model_dir $MODEL_DIR \
+                  --data_dir PhoATIS \
+                  --seed $s \
+                  --do_train \
+                  --do_eval \
+                  --save_steps 140 \
+                  --logging_steps 140 \
+                  --num_train_epochs 3 \
                   --tuning_metric mean_intent_slot \
                   --attention_embedding_size 200 \
                   --use_crf \
