ebcbd349d1ec0ef49d6fa4a5515b9a3b491624e5
baochi0212 <chideptraika29@gmail.com>
2022-11-01 09:39:44 +0700

    JointBert

--- docs/src/JointIDSF/run_debug.sh
+++ docs/src/JointIDSF/run_debug.sh
@@ -27,6 +27,7 @@ python3 main.py --token_level word-level \
                   --embedding_type soft \
                   --intent_loss_coef $c \
                   --learning_rate $lr
+                  --use_crf \
 
 python3 main.py --token_level word-level \
                   --model_type phobert \
@@ -39,3 +40,9 @@ python3 main.py --token_level word-level \
                   --logging_steps 140 \
                   --num_train_epochs $e\
                   --tuning_metric loss \
+                  --attention_embedding_size 200 \
+                  --use_crf \
+                  --gpu_id 0 \
+                  --embedding_type soft \
+                  --intent_loss_coef $c \
+                  --learning_rate $lr
