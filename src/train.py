49f84c79253e1ca2cfa99fee3e079a4028f78a62
baochi0212 <chideptraika29@gmail.com>
2022-11-12 21:23:05 +0700

    BertGCN

--- src/train.py
+++ src/train.py
@@ -292,6 +292,105 @@ def metrics_pipeline(mapping, start, end, l_start, l_end):
     print("COUNT", count)
     return count/batch_size
 
+def train_QA(model, optimizer, scheduler, train_dataloader, total_steps, epochs, val_dataloader=None, evaluation=False, overfit_batch=False):
+    """
+    -Set the val to None: if don't wanna keep track of validation.
+    -Overfit one batch: for check sanity of model
+    -init model before training
+    """
+    # def init_weights(m):
+    #     if isinstance(m, nn.Linear):
+    #         torch.nn.init.xavier_uniform(m.weight)
+    #         m.bias.data.fill_(0.01)
+    # model.apply(init_weights)
+    #for overfit
+    fixed_batch = next(iter(train_dataloader))
+    # Start training loop
+    print("Start training...\n")
+    run = tqdm(range(total_steps))
+    for epoch_i in range(epochs):
+        # =======================================
+        #               Training
+        # =======================================
+        # Print the header of the result table
+        print(f"{'Epoch':^7} | {'Batch':^7} | {'Train Loss':^12} | {'Val Loss':^10} | {'Val Acc':^9} | {'Elapsed':^9}")
+        print("-"*70)
+
+        # Measure the elapsed time of each epoch
+        t0_epoch, t0_batch = time.time(), time.time()
+
+        # Reset tracking variables at the beginning of each epoch
+        total_loss, batch_loss, batch_counts = 0, 0, 0
+
+        # Put the model into the training mode
+        model.train()
+
+        # For each batch of training data...
+        for step, batch in enumerate(train_dataloader):
+            batch_counts +=1
+            # Load batch to GPU
+            '''
+            NOTICE: CUSTOM HERE!!!
+            START 
+            '''
+            if overfit_batch:
+                batch = fixed_batch
+            b_input_ids, b_attn_mask, b_end, b_start = tuple(t.to(device) for t in batch)
+
+            # Zero out any previously calculated gradients
+            optimizer.zero_grad()
+            # print(b_input_ids.shape, b_attn_mask.shape)
+            # Perform a forward pass. This will return logits.
+            loss, _ = model(b_input_ids, b_attn_mask, b_end, b_start)
+            batch_loss += loss.item()
+            total_loss += loss.item()
+            # Perform a backward pass to calculate gradients
+            loss.backward()
+            '''
+            END!!!! (MODIFY THE VAL LOADER AS WELL, and maybe LOSS PRINTER)
+            '''
+
+            # Clip the norm of the gradients to 1.0 to prevent "exploding gradients"
+#             torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+            # Update parameters and the learning rate
+            optimizer.step()
+            # scheduler.step()
+            run.update(1)
+            
+
+            # Print the loss values and time elapsed for every 20 batches
+            if (step % 20 == 0 and step != 0) or (step == len(train_dataloader) - 1):
+                # Calculate time elapsed for 20 batches
+                time_elapsed = time.time() - t0_batch
+
+                # Print training results
+                print(f"{epoch_i + 1:^7} | {step:^7} | {batch_loss / batch_counts:^12.6f}| {'-':^10} | {'-':^9} | {time_elapsed:^9.2f}")
+
+                # Reset batch tracking variables
+                batch_loss , batch_counts = 0, 0
+                t0_batch = time.time()
+
+        # Calculate the average loss over the entire training data
+        avg_train_loss = total_loss/len(train_dataloader)
+
+        print("-"*70)
+        # =======================================
+        #               Evaluation
+        # =======================================
+        if evaluation == True:
+            # After the completion of each training epoch, measure the model's performance
+            # on our validation set.
+            val_loss, val_accuracy = evaluate_QA(model, val_dataloader)
+
+            # Print performance over the entire training data
+            time_elapsed = time.time() - t0_epoch
+            
+            print(f"{epoch_i + 1:^7} | {'-':^7} | {avg_train_loss:^12.6f}| {val_loss:^10.6f} | {val_accuracy:^9.2f} | {time_elapsed:^9.2f}")
+            print("-"*70)
+        print("\n")
+    print("Training complete!")
+
 def evaluate_QA(model, val_dataloader, print_fn=False, test=False, pipeline=False):
     """After the completion of each training epoch, measure the model's performance
     on our validation set.
@@ -362,10 +461,13 @@ if __name__ == '__main__':
     # scheduler = transformers.get_linear_schedule_with_warmup(optimizer,
     #                                             num_warmup_steps=0, # Default value
     #                                             num_training_steps=total_steps)
-    test_df = pd.read_csv(qa_processed + '/dev.csv')
-    test_dataset = QADataset(test_df, tokenizer=tokenizer, mode='test')
+    train_df = pd.read_csv(qa_processed + '/train.csv')
+    val_df = pd.read_csv(qa_processed + '/dev.csv')
+    test_df = pd.read_csv(qa_processed + '/test.csv')
+    train_dataset = QADataset(test_df, tokenizer=tokenizer, mode='train')
+    test_dataset = QADataset(test_df, tokenizer=tokenizer, mode='dev')
     test_loader = data.DataLoader(test_dataset, batch_size=batch_size)
-    print(evaluate_QA(model.to(device), test_loader, test=True))
+    print(evaluate_QA(model.to(device), test_loader, test=False))
     # for i in range(len(test_loader)):
     #     print(next(iter(test_loader))[-1].shape)
     # test_df = pd.read_csv(qa_processed + '/test.csv')
