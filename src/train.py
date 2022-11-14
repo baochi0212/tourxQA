50d3d4abe5cd4057b4e461accb294d8450e7c263
baochi0212 <chideptraika29@gmail.com>
2022-11-14 15:31:43 +0700

    IDSF

--- src/train.py
+++ src/train.py
@@ -433,9 +433,9 @@ def evaluate_QA(model, val_dataloader, device, tokenizer=tokenizer, print_fn=Fal
                 start_logits = outputs['start_logits']
                 end_logits  = outputs['end_logits']
                 start, end = torch.argmax(start_logits, -1), torch.argmax(end_logits, -1)
-                if count == 1:
-                    print("Q and C: ", tokenizer.decode(b_input_ids[0]))
-                    print("answers:", tokenizer.decode(b_input_ids[0][start[0]:end[0]+1]))
+                # if count == 1:
+                #     print("Q and C: ", tokenizer.decode(b_input_ids[0]))
+                #     print("answers:", tokenizer.decode(b_input_ids[0][start[0]:end[0]+1]))
                 #in squad-v2 val is same as train (1-label) but in Vi-squad it's multi-label
                 val_accuracy.append(metrics(start, end, b_start, b_end, metrics='acc', test=True))
           val_loss =  np.array(val_loss).mean()
