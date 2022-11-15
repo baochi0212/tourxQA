6293baadb4f4c0e4bf6756af9099516845a5f792
baochi0212 <chideptraika29@gmail.com>
2022-11-15 17:57:24 +0700

    IDSF

--- src/predict.py
+++ src/predict.py
@@ -20,6 +20,7 @@ from dataset.test_dataset import IntentPOSDataset, QADataset
 from models.modules import IntentPOSModule, CRFPOS, CustomConfig, QAModule
 from utils.preprocess import get_label
 from train import evaluate_QA
+import argparse
 
 data_dir = os.environ['dir']
 raw_dir = data_dir + '/data/raw/PhoATIS'
@@ -31,12 +32,21 @@ qa_processed = data_dir + '/data/processed/QA'
 tokenizer_checkpoint = 'NlpHUST/bert-base-vn'
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--n_epochs', default=9, type=int, help='Number of epochs')
+parser.add_argument('--batch_size', default=32, type=int, help='Batch size')
+parser.add_argument('--learning_rate', default=3e-5, type=float, help="Learning rate")
+parser.add_argument('--pretrained_model', default='NlpHUST/bert-base-vn', type=str)
+parser.add_argument('--pretrained_input', default=768, type=int)
+
 if __name__ == '__main__':
-    batch_size = 16
+    args = parser.parse_args()
+    batch_size = args.batch_size
+
     device  = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model_checkpoint = 'NlpHUST/bert-base-vn'
+    model_checkpoint = args.pretrained_model
     model = QAModule(model_checkpoint=model_checkpoint, device=device).to(device)
-    tokenizer = AutoTokenizer.from_pretrained(tokenizer_checkpoint)
+    tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
     # model = AutoModelForQuestionAnswering.from_pretrained(checkpoint)
     model_path = './models/weights/model.pt'
 
