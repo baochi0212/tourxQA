a807e94f703c73e013a1750e5547f17a5dde3d4e
baochi0212 <chideptraika29@gmail.com>
2022-11-19 15:57:02 +0700

    IDSF

--- src/train.py
+++ src/train.py
@@ -494,8 +494,8 @@ if __name__ == '__main__':
     tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
     model = QAModule(model_checkpoint=model_checkpoint, device=device, hidden=args.pretrained_input, config=config, args=args).from_pretrained(
         model_checkpoint,
-        config,
-        args
+        config=config,
+        args=args
     )
     optimizer = transformers.AdamW(model.parameters(), lr=lr, weight_decay=0.01)
     model_path = './models/weights/model.pt'
