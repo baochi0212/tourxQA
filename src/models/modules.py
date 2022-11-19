786275230bd8e7f5631f63cf0a3102bf60f62bf9
baochi0212 <chideptraika29@gmail.com>
2022-11-19 16:05:08 +0700

    IDSF

--- src/models/modules.py
+++ src/models/modules.py
@@ -60,7 +60,7 @@ class CRFPOS(nn.Module):
         crf_pos = nn.functional.relu(self.pos_head(x['last_hidden_state']))
         return torch.sigmoid(nn.functional.relu(self.intent_head(x['last_hidden_state'].mean(dim=1)))), crf_pos, -self.CRF(crf_pos.permute(1, 0, 2), pos_label.permute(1, 0))
 class QAModule(RobertaPreTrainedModel):
-  def __init__(self, config, model_checkpoint, device, args=None, hidden=768, out=386):
+  def __init__(self, config, device, args=None, hidden=768, out=386):
     super().__init__(config)
     def CE_loss_fn(pred, label):
     #     print("pred", pred.shape)
