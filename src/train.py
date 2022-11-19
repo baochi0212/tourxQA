4fc94df25c0ea7b443c75303b359fd0a037ac4be
baochi0212 <chideptraika29@gmail.com>
2022-11-19 15:59:33 +0700

    IDSF

--- src/train.py
+++ src/train.py
@@ -493,9 +493,10 @@ if __name__ == '__main__':
     config = RobertaConfig.from_pretrained(model_checkpoint)
     tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
     model = QAModule(model_checkpoint=model_checkpoint, device=device, hidden=args.pretrained_input, config=config, args=args).from_pretrained(
-        model_checkpoint,
+        model_checkpoint=model_checkpoint,
         config=config,
-        args=args
+        args=args,
+        device=device
     )
     optimizer = transformers.AdamW(model.parameters(), lr=lr, weight_decay=0.01)
     model_path = './models/weights/model.pt'
