5d4cb98070ee2c5a3e8343e3ba7d1a27d62530cd
baochi0212 <chideptraika29@gmail.com>
2022-11-19 22:35:19 +0700

    IDSF

--- src/models/modules.py
+++ src/models/modules.py
@@ -12,7 +12,7 @@ import pandas as pd
 from transformers import AutoModel, AutoTokenizer, RobertaModel
 from dataset.test_dataset import IntentPOSDataset
 import transformers
-from transformers import AutoModelForQuestionAnswering, AutoTokenizer, AdamW, get_linear_schedule_with_warmup, XLMRobertaPreTrainedModel, XLMRobertaModel
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer, AdamW, get_linear_schedule_with_warmup, RobertaPreTrainedModel, XLMRobertaModel, RobertaModel
 from dataset.test_dataset import IntentPOSDataset, QADataset
 from utils.preprocess import get_label
 data_dir = os.environ['dir']
@@ -75,7 +75,7 @@ class CRFPOS(nn.Module):
 #     self.relu = torch.nn.ReLU()
 #     self.loss_fn = CE_loss_fn
 #     # self.init_weights()
-class QAModule(XLMRobertaPreTrainedModel):
+class QAModule(RobertaPreTrainedModel):
   def __init__(self, config, device, args=None, hidden=768):
     super().__init__(config)
     def CE_loss_fn(pred, label):
@@ -84,7 +84,7 @@ class QAModule(XLMRobertaPreTrainedModel):
         loss = torch.where(label != 0, loss, torch.tensor(0, dtype=torch.float).to(device))
         return loss.mean()
     # self.bert_model = AutoModel.from_pretrained("xlm-roberta-base")
-    self.bert_model = XLMRobertaModel(config)
+    self.bert_model = RobertaModel(config)
     self.args = args
     # self.bert_qa = AutoModelForQuestionAnswering.from_pretrained('nguyenvulebinh/vi-mrc-large')
     self.pretrained = self.args.fast
