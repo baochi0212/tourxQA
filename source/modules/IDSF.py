11f2ca21b9a60b2def95fb1b08b3c7cb1ad28e75
baochi0212 <chideptraika29@gmail.com>
2022-11-26 12:01:13 +0700

    idea

---
+++ source/modules/IDSF.py
@@ -0,0 +1,111 @@
+import sys
+import os
+sys.path.append(os.environ['source'])
+import numpy as np
+
+import torch
+import torch.nn
+from transformers import AdamW, get_linear_schedule_with_warmup
+
+from utils import MODEL_DICT, get_intent_labels, get_slot_labels, load_tokenizer
+from data_loader import load_and_cache_examples
+from module import Module
+from main import args
+
+
+
+class ISDFModule(Module):
+    def __init__(self, args):
+        super().__init__(args)
+        #define model
+        config, _, model  = MODEL_DICT[args.pretrained_model]
+        self.intent_label_lst, self.slot_label_lst = get_intent_labels(args), get_slot_labels(args)
+       
+        if args.pretrained:
+            self.config = config.from_pretrained(args.pretrained_model)
+            self.model = self.model_class.from_pretrained(
+                args.pretrained_model,
+                args=args,
+                intent_label_lst=self.intent_label_lst,
+                slot_label_lst=self.slot_label_lst,
+            )
+        else:
+            self.model = model
+        #define hparams
+        self.batch_size = args.batch_size
+        self.lr = args.learning_rate
+        self.n_epochs = args.n_epochs
+    
+    def train_step(self, batch):
+        batch = tuple(t.to(self.device) for t in batch)  # GPU or CP
+        inputs = {
+            "input_ids": batch[0],
+            "attention_mask": batch[1],
+            "intent_label_ids": batch[3],
+            "slot_labels_ids": batch[4],
+        }
+        if "distill" not in args.pretrained_model:
+            inputs["token_type_ids"] = batch[2]
+        outputs = self.model(**inputs)
+        loss = outputs[0]
+        if self.args.gradient_accumulation_steps > 1:
+            loss = loss / self.args.gradient_accumulation_steps
+        
+        return {"loss": loss}
+
+    def val_step(self, batch):
+        batch = tuple(t.to(self.device) for t in batch)
+        with torch.no_grad():
+            inputs = {
+                "input_ids": batch[0],
+                "attention_mask": batch[1],
+                "intent_label_ids": batch[3],
+                "slot_labels_ids": batch[4],
+            }
+            if "distill" not in args.pretrained_model:
+                inputs["token_type_ids"] = batch[2]
+            outputs = self.model(**inputs)
+            tmp_eval_loss, (intent_logits, slot_logits) = outputs[:2]
+            loss = tmp_eval_loss.mean().item()
+        # Intent prediction
+        if intent_preds is None:
+            intent_preds = intent_logits.detach().cpu().numpy()
+            intent_label_ids = inputs["intent_label_ids"].detach().cpu().numpy()
+        else:
+            intent_preds = np.append(intent_preds, intent_logits.detach().cpu().numpy(), axis=0)
+            intent_label_ids = inputs["intent_label_ids"].detach().cpu().numpy()
+        # Slot prediction
+        if slot_preds is None:
+            if self.args.use_crf:
+                # decode() in `torchcrf` returns list with best index directly
+                slot_preds = np.array(self.model.crf.decode(slot_logits))
+            else:
+                slot_preds = slot_logits.detach().cpu().numpy()
+            slot_label_ids = inputs["slot_labels_ids"].detach().cpu().numpy()
+        else:
+            if self.args.use_crf:
+                slot_preds = np.append(slot_preds, np.array(self.model.crf.decode(slot_logits)), axis=0)
+            else:
+                slot_preds = np.append(slot_preds, slot_logits.detach().cpu().numpy(), axis=0)
+            slot_label_ids = inputs["slot_labels_ids"].detach().cpu().numpy()
+
+        return {"loss": loss, "intent": intent_label_ids, "slot": slot_label_ids}
+
+if __name__ == "__main__":
+    module = ISDFModule(args)
+    tokenizer = load_tokenizer(args)
+    data = load_and_cache_examples(args, tokenizer, mode="train")
+
+    
+
+
+
+        
+
+
+        
+
+
+        
+
+
---
+++ source/modules/module.py
@@ -0,0 +1,32 @@
+from transformers import AutoTokenizer, AutoModel
+from main import args
+import torch.nn as nn
+import torch
+from torch.utils import data
+MODEL_DICT = {'vinai/phobert-base': []}
+class Module:
+    def __init__(self, args):
+        self.args = args
+        #define modelling, example:
+        # self.model, self.pretrained_path = MODEL_DICT[args.model_type] 
+        #define hyper-params, example:
+        # self.batch_size = args.batch_size
+        # self.optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
+
+    def train_step(self, train_loader):
+        pass
+    def val_step(self, val_loader):
+        pass 
+    def test_step(self, test_loader):
+        pass
+    def configure_optimizer(self):
+        #optimizer, scheduler
+        pass
+    def loader(self, dataset, collate=False):
+        pass 
+    
+
+
+
+
+    
