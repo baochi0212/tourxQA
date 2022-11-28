a2da3f94882709fc886e51495fce65afbe1a88da
baochi0212 <chideptraika29@gmail.com>
2022-11-28 22:43:40 +0700

    PL mimics

--- source/model/IDSF_modules/joint_rnn.py
+++ source/model/IDSF_modules/joint_rnn.py
@@ -91,7 +91,7 @@ class JointLSTM(nn.Module):
                     slot_loss = slot_loss_fct(active_logits, active_labels)
                 else:
                     slot_loss = slot_loss_fct(slot_logits.view(-1, self.num_slot_labels), slot_labels_ids.view(-1))
-            total_loss = (1 - self.args.intent_loss_coef) * slot_loss
+            total_loss += (1 - self.args.intent_loss_coef) * slot_loss
 
         outputs = ((intent_logits, slot_logits),) # add hidden states and attention if they are here
 
