4a89ac72e15e322bd8c2da0e9e8981556079a015
baochi0212 <chideptraika29@gmail.com>
2022-11-29 19:00:02 +0700

    PL mimics]

--- source/predict.py
+++ source/predict.py
@@ -4,7 +4,7 @@ from trainer import *
 
 if __name__ == '__main__':
     module = ISDFModule(args)
-    tokenizer = load_tokenizer(args.pretrained_model)
+    tokenizer = load_tokenizer(args)
     test_dataset = load_and_cache_examples(args, tokenizer, mode="test")
     trainer = Trainer(args, module)
     trainer.predict(test_dataset)
