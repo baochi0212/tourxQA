16e98902b7ca4d35ad5a83560f0cde70f7c087f5
baochi0212 <chideptraika29@gmail.com>
2022-11-29 21:11:41 +0700

    PL mimics]

--- source/predict.py
+++ source/predict.py
@@ -29,6 +29,7 @@ def load_model(args, device):
     try:
         load_dir = args.idsf_model_dir + f"/{args.model_type}_{int(args.n_epochs)}_{args.learning_rate}.pt"
         config, _, model = MODEL_DICT[args.model_type]
+        config = config.from_pretrained(args.pretrained_model)
         model = model(config, args=args, intent_label_lst=get_intent_labels(args), slot_label_lst=get_slot_labels(args))
         model.load_state_dict(
 
