4365acc49be2066a0e2cd39cf4f922ca4c35798c
baochi0212 <chideptraika29@gmail.com>
2022-11-29 13:43:54 +0700

    PL mimics

--- source/predict.py
+++ source/predict.py
@@ -2,7 +2,7 @@ from trainer import *
 
 
 if __name__ == '__main__':
+    module = ISDFModule(args)
     trainer = Trainer(args, module)
-    test_dataset = load_and_cache_examples(args, tokenizer, mode="test")
-    trainer.eval(test_dataset, mode="test")
+    trainer.predict()
 
