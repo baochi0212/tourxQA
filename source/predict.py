943f0983cf54a186ec70b99e1a6869a140b0f5b6
baochi0212 <chideptraika29@gmail.com>
2022-12-06 11:06:01 +0700

    distillation

--- source/predict.py
+++ source/predict.py
@@ -226,7 +226,7 @@ if __name__ == "__main__":
             tokenizer = load_tokenizer(args)
             test_dataset = load_and_cache_examples(args, tokenizer, mode="test")
             trainer = Trainer_IDSF(args, module)
-            trainer.eval(test_dataset, mode="test")
+            trainer.predict(test_dataset)
     else:
         pass
 
