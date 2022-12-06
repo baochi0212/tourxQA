46cfe79b9b736bdbdd0b42b4e5ee73b9f0ca7e3b
baochi0212 <chideptraika29@gmail.com>
2022-12-06 10:45:21 +0700

    distillation

--- source/runtime/git_script.sh
+++ source/runtime/git_script.sh
@@ -1,5 +1,5 @@
-!git config --global user.email "chideptraiak29@gmail.com"
-!git config --global user.name "baochi0212"
-!git add . 
-!git commit -m "save result"
-!git push 
+git config --global user.email "chideptraiak29@gmail.com"
+git config --global user.name "baochi0212"
+git add . 
+git commit -m "save result"
+git push 
