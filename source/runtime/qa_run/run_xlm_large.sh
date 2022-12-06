4bcac2ed4b23a1f9671ada030cc5d01dc7121a65
baochi0212 <chideptraika29@gmail.com>
2022-12-06 20:15:52 +0700

    distillation

--- source/runtime/qa_run/run_xlm_large.sh
+++ source/runtime/qa_run/run_xlm_large.sh
@@ -1,1 +1,1 @@
-!python trainer.py --pretrained --model_type xlm-roberta-large --pretrained_model xlm-roberta-large --device cuda --task QA --train_batch_size 4 --eval_batch_size 32 --learning_rate 3e-6 --logging_steps 2000
+python trainer.py --pretrained --model_type xlm-roberta-large --pretrained_model xlm-roberta-large --device cuda --task QA --train_batch_size 4 --eval_batch_size 32 --learning_rate 3e-6 --logging_steps 2000
