a6bd1ee16f3e4c03ba779377245b6fbea549619d
baochi0212 <chideptraika29@gmail.com>
2022-12-08 21:07:02 +0700

    run debug

---
+++ source/runtime/qa_run/run_electra.sh
@@ -0,0 +1,1 @@
+python trainer.py --pretrained --model_type electra --pretrained_model NlpHUST/electra-base-vn --device cuda --task QA --train_batch_size 16 --eval_batch_size 32 --logging_step 1000
--- source/runtime/qa_run/run_xlm_base.sh
+++ source/runtime/qa_run/run_xlm_base.sh
@@ -1,1 +1,1 @@
-python trainer.py --pretrained --model_type xlm-roberta-base --pretrained_model xlm-roberta-base --device cuda --task QA --train_batch_size 16 --eval_batch_size 32 --logging_step 500
+python trainer.py --pretrained --model_type xlm-roberta-base --pretrained_model xlm-roberta-base --device cuda --task QA --train_batch_size 16 --eval_batch_size 32 --logging_step 1000
