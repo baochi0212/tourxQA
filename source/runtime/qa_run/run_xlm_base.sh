aa869dee4f8dc8feda566a5d2e38602782375dc4
baochi0212 <chideptraika29@gmail.com>
2022-12-09 21:40:57 +0700

    run debug

--- source/runtime/qa_run/run_xlm_base.sh
+++ source/runtime/qa_run/run_xlm_base.sh
@@ -1,1 +1,3 @@
-python trainer.py --pretrained --model_type xlm-roberta-base --pretrained_model xlm-roberta-base --device cuda --task QA --train_batch_size 16 --eval_batch_size 32 --tuning_metric F1_score --logging_step 1000
+#additional 
+freeze_weights = $1
+python trainer.py --freeze $freeze_weights --pretrained --model_type xlm-roberta-base --pretrained_model xlm-roberta-base --device cuda --task QA --train_batch_size 16 --eval_batch_size 32 --tuning_metric F1_score --logging_step 1000
--- source/runtime/qa_run/run_xlm_large.sh
+++ source/runtime/qa_run/run_xlm_large.sh
@@ -1,1 +1,3 @@
-python trainer.py --pretrained --model_type xlm-roberta-large --pretrained_model xlm-roberta-large --device cuda --task QA --train_batch_size 4 --eval_batch_size 32 --learning_rate 3e-6 --logging_steps 1000
+#additional 
+freeze_weights = $1
+python trainer.py --freeze $freeze_weights --pretrained --model_type xlm-roberta-large --pretrained_model xlm-roberta-large --device cuda --task QA --train_batch_size 4 --eval_batch_size 32 --learning_rate 3e-6 --logging_steps 1000
