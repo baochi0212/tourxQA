1683cc275516f1082bc0df94f4b605636a960870
baochi0212 <chideptraika29@gmail.com>
2022-12-10 12:24:47 +0700

    freeze bert

--- source/runtime/qa_run/run_electra.sh
+++ source/runtime/qa_run/run_electra.sh
@@ -1,4 +1,4 @@
 #additional 
 export freeze_weights=$1
 echo "Freeze ${freeze_weights}"
-python trainer.py --freeze $freeze_weights --pretrained --model_type electra --pretrained_model NlpHUST/electra-base-vn --device cuda --task QA --train_batch_size 16 --eval_batch_size 32 --tuning_metric F1_score --logging_step 1000
+python trainer.py --freeze "${freeze_weights}" --pretrained --model_type electra --pretrained_model NlpHUST/electra-base-vn --device cuda --task QA --train_batch_size 16 --eval_batch_size 32 --tuning_metric F1_score --logging_step 1000
