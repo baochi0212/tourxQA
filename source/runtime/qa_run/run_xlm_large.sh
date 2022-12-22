aa720ae23aa4fd928c78170d8be69781376a113d
baochi0212 <chideptraika29@gmail.com>
2022-12-22 19:18:23 +0700

    ...g

--- source/runtime/qa_run/run_xlm_large.sh
+++ source/runtime/qa_run/run_xlm_large.sh
@@ -1,3 +1,3 @@
 #additional 
 export freeze_weights=$1
-python trainer.py --freeze $freeze_weights --pretrained --model_type xlm-roberta-large --pretrained_model xlm-roberta-large --device cuda --task QA --train_batch_size 4 --eval_batch_size 32 --learning_rate 3e-5 --logging_steps 2500 --tuning_metric F1_score
+python trainer.py --freeze $freeze_weights --pretrained --model_type xlm-roberta-large --pretrained_model xlm-roberta-large --device cuda --task QA --train_batch_size 4 --eval_batch_size 32 --learning_rate 3e-6 --logging_steps 1000 --tuning_metric F1_score
