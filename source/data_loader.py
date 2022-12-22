75dc5499e2f12c565063f83bf988cddcd00fd1e3
baochi0212 <chideptraika29@gmail.com>
2022-12-22 14:35:32 +0700

    ...g

--- source/data_loader.py
+++ source/data_loader.py
@@ -280,7 +280,7 @@ def load_and_cache_examples(args, tokenizer, mode):
     if os.path.exists(cached_features_file):
         logger.info("Loading features from cached file %s", cached_features_file)
         features = torch.load(cached_features_file)
-        print("EXISTED")
+        print("CLGT")
     else:
         # Load data features from dataset file
         print("CREATE NEW")
