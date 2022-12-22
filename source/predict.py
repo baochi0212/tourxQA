4fcbce5438052955e9a085dba7bec8cf0c4fe398
baochi0212 <chideptraika29@gmail.com>
2022-12-22 09:43:32 +0700

    ...g

--- source/predict.py
+++ source/predict.py
@@ -216,6 +216,7 @@ def predict_IDSF(args):
     with open(args.output_file, "w", encoding="utf-8") as f:
         for words, slot_preds, intent_pred in zip(lines, slot_preds_list, intent_preds):
             line = ""
+            print("word", words)
             for word, pred in zip(words, slot_preds):
                 if pred == "O":
                     line = line + word + " "
