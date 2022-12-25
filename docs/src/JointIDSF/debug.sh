bf177d3d441a9cc8f3f9dac7013e03dc68635d03
baochi0212 <chideptraika29@gmail.com>
2022-12-25 11:15:25 +0700

    ...g

--- docs/src/JointIDSF/debug.sh
+++ docs/src/JointIDSF/debug.sh
@@ -1,9 +1,12 @@
-export lr=3e-5
-export c=0.6
+#As we initialize JointIDSF from JointBERT, user need to train a base model JointBERT first
+./run_jointBERT-CRF_PhoBERTencoder.sh
+#Train JointIDSF
+export lr=4e-5
+export c=0.15
 export s=100
 echo "${lr}"
-export MODEL_DIR=JointBERT-CRF_PhoBERTencoder
-export MODEL_DIR=$MODEL_DIR"/"$lr"/"$c"/"$s
+export MODEL_DIR=JointIDSF_PhoBERTencoder
+export MODEL_DIR="${MODEL_DIR}/${lr}/${c}/${s}"
 echo "${MODEL_DIR}"
 python3 main.py --token_level word-level \
                   --model_type phobert \
@@ -14,10 +17,14 @@ python3 main.py --token_level word-level \
                   --do_eval \
                   --save_steps 140 \
                   --logging_steps 140 \
-                  --num_train_epochs 10 \
+                  --num_train_epochs 20 \
                   --tuning_metric mean_intent_slot \
+                  --use_intent_context_attention \
+                  --attention_embedding_size 200 \
                   --use_crf \
                   --gpu_id 0 \
                   --embedding_type soft \
                   --intent_loss_coef $c \
+                  --pretrained \
+                  --pretrained_path JointBERT-CRF_PhoBERTencoder/3e-5/0.6/100 \
                   --learning_rate $lr
