376e33e60aec37074995ebccf54c4473a3c5be80
baochi0212 <chideptraiak29@gmail.com>
2023-02-15 10:14:03 +0700

    google

--- crawler/googler/query.py
+++ crawler/googler/query.py
@@ -3,8 +3,10 @@ import os
 import json
 import argparse
 from googlesearch import search
+from transformers import AutoTokenizer
 
-
+tokenizer = AutoTokenizer.from_pretrained('xlm-roberta-base')
+sample_input_dir = f"{os.environ['dir']}/source/sample_input.txt"
 # from haystack.document_stores import ElasticsearchDocumentStore
 # from haystack.utils import convert_files_to_docs
 # from haystack.nodes import BM25Retriever
@@ -24,8 +26,6 @@ parser.add_argument('--query', type=str, default='đà nẵng có món gì ngon 
 # # Now, let's write the docs to our DB.
 # docs = convert_files_to_docs(dir_path=database_dir, split_paragraphs=True)
 # document_store.write_documents(docs)
-
-
 
 
 class Crawl:
@@ -77,29 +77,60 @@ class Crawl:
 
         f_write.close()
 
+    def to_input(self, query, json_file):
+        data = json.load(open(json_file, 'r'))
+        corpus = ' '.join([data[i]['content'] for i in range(len(data))])
+        tokens = tokenizer(query,
+                           corpus,
+                           max_length=512,
+                           truncation='only_second',
+                           stride=50,
+                           return_overflowing_tokens=True,
+                           )
+        print("INPUT_IDS", len(tokens['input_ids'][0]))
+        #can only add if written
+        f_write = open(sample_input_dir, 'w')
+        for input_ids in tokens['input_ids']:
+            input = tokenizer.decode(input_ids)
+            print("TEXT", input)
+            input = input.replace('</s></s>', '[SEP]')
+            input = input.replace('<s>', '')
+            f_write.write(input + '\n')
+        f_write.close()
+
+
+
+
+
+
+
         
+
     def query(self, q="Da Nang co mon gi ngon", num_results=3):
+        #urls.txt have been reset in self.crawl() method
         #save urls queried
         for i, result in enumerate(search(q + " " + self.name, num_results=num_results)):
             #only .chn 
             if result[-3:] == 'chn':
-                with open(f"{database_dir}/test/urls.txt", "a") as f:
-                    f.write(result + '\n')
+                with open(f"{database_dir}/test/urls.txt", "a") as f_write:
+                    f_write.write(result + '\n')
+        f_write.close()
 
         #crawl
-        # self.crawl()
-        # self.json2txt()
+        self.crawl()
+        self.to_input(q, f"{database_dir}/test/text.json")
                 
-            
     def crawl(self):
         current_docs = len(glob(f"{database_dir}/test/*.json"))
         print("num", current_docs)
-        os.system(f"scrapy runspider crawler/spiders/{self.name}.py -o {database_dir}/test/text{current_docs + 1}.json")
+        os.system(f"scrapy runspider {working_dir}/crawler/crawler/spiders/{self.name}.py -o {database_dir}/test/text.json")
         
         
-        #reset url-file after one crawling session
-        open(f"{database_dir}/test/urls.txt", "w")
-
+        #reset url-file and input-file after one crawling session
+        f_write = open(f"{database_dir}/test/urls.txt", "w")
+        f_write.close()
+        f_write = open(f"{database_dir}/test/urls.txt", "w")
+        f_write.close()
 
 
 
@@ -107,7 +138,7 @@ class Crawl:
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    Crawl("kenh14").query(q=args.query, num_results=1)
+    Crawl("kenh14").query(q=args.query, num_results=3)
     # q="mon an ngon Da Nang" 
     # num_results=3
     # for i, result in enumerate(search(q + " " + 'kenh14', num_results=num_results)):
