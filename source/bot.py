485771f850508922c708d366ffafa7624c74f2c5
baochi0212 <chideptraiak29@gmail.com>
2023-02-15 18:08:06 +0700

    google

--- source/bot.py
+++ source/bot.py
@@ -19,13 +19,14 @@ def send_welcome(message):
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Let's working man")
+
 @bot.message_handler(commands=['command_travelling'])
 def get_input(message):
     #send question
     text = "What is your command?"
     sent_msg = bot.send_message(message.chat.id, text)
     #message, callback, *args  
-    bot.register_next_step_handler(sent_msg, main)
+    bot.register_next_step_handler(sent_msg, main_IDSF)
 
 @bot.message_handler(commands=['ask_travelling'])
 def get_input(message):
